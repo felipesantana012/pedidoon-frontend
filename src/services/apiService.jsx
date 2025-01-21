fc72cd82213bef2c6ace82f7df1ced5dd4b5042f
Felipe Santana <felipe012santana012@gmail.com>
2025-01-21 13:11:36 -0300

    Alteração na BASE_URL

--- src/services/apiService.jsx
+++ src/services/apiService.jsx
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:3000";
+export const BASE_URL = "https://pedidoonbackend.felipedev.app.br";
 
 const getHeaders = () => {
   const token = localStorage.getItem("token");
